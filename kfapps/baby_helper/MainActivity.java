56eb7cf9562aeae00a202d4be1579d2ba878bb87
KristopherFennerSchool <105348396+KristopherFennerSchool@users.noreply.github.com>
2022-06-26 05:55:48 -0400

    Update MainActivity.java

--- kfapps/baby_helper/MainActivity.java
+++ kfapps/baby_helper/MainActivity.java
@@ -1,29 +1,28 @@
 package com.kfapps.baby_helper;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Activity;
-import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.sql.Array;
 
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity
 {
-    private FeedingDB dbhandler;
-    private EditText editText;
+    EditText DateOfFeeding, TimeOfFeeding, AmountOfFeeding;
+    Button SaveFeeding, ShowFeeding;
+    String FeedingDate, FeedingTime, FeedingAmount;
+    SQLiteDatabase SQLITEDATABASE;
+    Boolean CheckEditTextEmpty;
+    String SQLiteQuery;
     private CalendarView calenderView;
     private String selectedDate;
-    private SQLiteDatabase sqLiteDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,58 +30,95 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.feedingDetails);
+        DateOfFeeding = (EditText) findViewById(R.id.editText1);
+        TimeOfFeeding = (EditText) findViewById(R.id.editText2);
+        AmountOfFeeding = (EditText) findViewById(R.id.editText3);
         calenderView = findViewById(R.id.calendarView);
+        SaveFeeding = (Button) findViewById(R.id.button1);
+        ShowFeeding = (Button) findViewById(R.id.button2);
 
-
+        //Adds Feeding to DB when SaveFeeding Button is clicked
+        SaveFeeding.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                DBCreate();
+                SubmitData2SQLiteDB();
+            }
+        });
+        //Grabs Date from selected calender converts to string and assigns to DateOfFeeding box
         calenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
         {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth)
             {
-                selectedDate = Integer.toString(year) + Integer.toString(month) + Integer.toString(dayOfMonth);
-                ReadDatabase(calendarView);
+                selectedDate = Integer.toString(month + 1) + "/" + Integer.toString(dayOfMonth) + "/" + Integer.toString(year);
+                DateOfFeeding.setText(selectedDate);
+
             }
         });
-        try
+        //Sends To GetDataActivity as well as the selected date as a string
+        ShowFeeding.setOnClickListener(new View.OnClickListener()
         {
-            dbhandler = new FeedingDB(this, "calendarDB", null, 1);
-            sqLiteDatabase = dbhandler.getWritableDatabase();
-            sqLiteDatabase.execSQL("CREATE TABLE FeedingCalendar(Date TEXT, Feeding TEXT)");
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
+            @Override
+            public void onClick(View view)
+            {
+                String RetrieveFeedingDate = selectedDate;
+                Intent intent = new Intent(MainActivity.this, GetDataActivity.class);
+                intent.putExtra("RetrieveFeedingDate", selectedDate);
+                startActivity(intent);
 
+            }
+        });
     }
 
-    public void AddFeedingDB (View view)
+    //opens or creates DB
+    public void DBCreate()
     {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("Date",selectedDate);
-        contentValues.put("Feeding", editText.getText().toString());
-        sqLiteDatabase.insert("FeedingCalendar", null,contentValues);
-
+        SQLITEDATABASE = openOrCreateDatabase("FeedingDatabase", Context.MODE_PRIVATE, null);
+        SQLITEDATABASE.execSQL("CREATE TABLE IF NOT EXISTS feedingTable(date_feeding VARCHAR PRIMARY KEY, time_feeding VARCHAR, amount_feeding VARCHAR);");
     }
 
-    public void ReadDatabase(View view)
+    //Submits entered data to DB on Save feeding click
+    public void SubmitData2SQLiteDB()
     {
-        String query = "Select Feeding from FeedingCalendar where Date =" + selectedDate;
-        try
+        FeedingDate = DateOfFeeding.getText().toString();
+        FeedingTime = TimeOfFeeding.getText().toString();
+        FeedingAmount = AmountOfFeeding.getText().toString();
+        CheckEditTextEmptyOrNot(FeedingDate, FeedingTime, FeedingAmount);
+
+
+        //Verifies all fields filled and prompts user accordingly
+        if(CheckEditTextEmpty == true)
         {
-            Cursor cursor = sqLiteDatabase.rawQuery(query, null,null);
-            cursor.moveToFirst();
-            editText.setText(cursor.getString(0));
+            SQLiteQuery = "INSERT INTO feedingTable (date_feeding, time_feeding, amount_feeding) VALUES('"+FeedingDate+"', '"+FeedingTime+"', '"+FeedingAmount+"');";
+            SQLITEDATABASE.execSQL(SQLiteQuery);
+            Toast.makeText(MainActivity.this, "Data Submit Successful", Toast.LENGTH_LONG).show();
+            ClearEditTextAfterDoneTask();
         }
-        catch (Exception e)
+        else
         {
-            e.printStackTrace();
-            editText.setText("");
+            Toast.makeText(MainActivity.this, "Please Fill All Fields", Toast.LENGTH_LONG).show();
         }
     }
-    public void viewDay(View view)
+    public void CheckEditTextEmptyOrNot (String FeedingDate,String FeedingTime, String FeedingAmount)
     {
-        ReadDatabase(calenderView);
+        if(TextUtils.isEmpty(FeedingDate) || TextUtils.isEmpty(FeedingTime) || TextUtils.isEmpty(FeedingAmount))
+        {
+            CheckEditTextEmpty = false;
+        }
+        else
+        {
+            CheckEditTextEmpty = true;
+        }
+    }
+    public void ClearEditTextAfterDoneTask()
+    {
+        DateOfFeeding.getText().clear();
+        TimeOfFeeding.getText().clear();
+        AmountOfFeeding.getText().clear();
     }
 }
+
+
